9aa101b9a95141d4f0386c975addd00a3ec35c1f
b9033970 <b9033970@newcastle.ac.uk>
2020-07-02 12:53:16 +0200

    created function putting tokenized words from .json file into certain lists

--- main.py
+++ main.py
@@ -11,3 +11,18 @@ with open('dialog.json') as file:
     data = json.load(file)
 
 
+words = []
+labels = []
+user_input = []
+words_labels = []
+
+for dialog in data['dialog']:
+    for pattern in dialog['patterns']:
+        words_list = nltk.word_tokenize(pattern)  # Separating each word and splitting symbols from them
+        words.extend(words_list)
+        user_input.append(pattern)
+        words_labels.append(dialog['tag'])  # Classifying each word
+
+    if dialog['tag'] not in labels:
+        labels.append(dialog['tag'])
+
